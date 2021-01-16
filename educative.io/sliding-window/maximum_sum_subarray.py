9d896bd14f0ecd4e2f9d0a45df2005be4e82d8d9
Sharat Doddaghatta <schandra2@godaddy.com>
2021-01-15 22:14:23 -0800

    working

---
+++ educative.io/sliding-window/maximum_sum_subarray.py
@@ -0,0 +1,19 @@
+import math
+
+
+def max_sub_array_of_size_k(k, arr):
+
+    sum = 0
+    max_sum = -math.inf
+
+    for i in range(len(arr)):
+        sum += arr[i]
+        if i >= k-1:
+            max_sum = max(max_sum, sum)
+            sum -= arr[i+1-k]
+    return max_sum
+
+
+if __name__ == "__main__":
+    print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(3, [2, 1, 5, 1, 3, 2])))
+    print("Maximum sum of a subarray of size K: " + str(max_sub_array_of_size_k(2, [2, 3, 4, 1, 5])))
