dea1873e4b4bdeabc16c610e701f4af1eec48ab9
Sharat Doddaghatta <schandra2@godaddy.com>
2021-01-15 23:43:30 -0800

    working

---
+++ educative.io/sliding-window/longest_substring_with_k_distinct.py
@@ -0,0 +1,30 @@
+import math
+from collections import Counter
+
+
+def longest_substring_with_k_distinct(str, k):
+
+    l = 0
+    char_frequency = Counter()
+    distinct = 0
+    max_length = 0
+
+    for r in range(len(str)):
+        if char_frequency[str[r]] == 0:
+            distinct += 1
+        char_frequency[str[r]] += 1
+
+        while distinct > k:
+            char_frequency[str[l]] -= 1
+            if char_frequency[str[l]] == 0:
+                distinct -= 1
+            l += 1
+        max_length = max(max_length, r - l+1)
+
+    return max_length
+
+
+if __name__ == '__main__':
+    print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 2)))
+    print("Length of the longest substring: " + str(longest_substring_with_k_distinct("araaci", 1)))
+    print("Length of the longest substring: " + str(longest_substring_with_k_distinct("cbbebi", 3)))
