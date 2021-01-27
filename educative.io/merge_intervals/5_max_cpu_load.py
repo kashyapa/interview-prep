af7aa8340328ff29c040ed60efb9c56777523c07
Sharat Doddaghatta <schandra2@godaddy.com>
2021-01-26 20:21:31 -0800

    26-jan

---
+++ educative.io/merge_intervals/5_max_cpu_load.py
@@ -0,0 +1,28 @@
+from heapq import *
+
+
+class job:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __lt__(self, other):
+        return self.end < other.end
+
+
+def find_max_cpu_loads(jobs):
+
+    jobs.sort(key=lambda x: x.start)
+
+    min_heap = []
+    max_cpu_load, current_cpu_load = 0, 0
+
+    for j in jobs:
+        while len(min_heap) > 0 and j.start >= min_heap[0].end:
+            current_cpu_load -= min_heap[0].cpu_load
+            heappop(min_heap)
+
+        heappush(min_heap, j)
+        current_cpu_load += j.cpu_load
+        max_cpu_load = max(max_cpu_load, current_cpu_load)
+    return max_cpu_load
