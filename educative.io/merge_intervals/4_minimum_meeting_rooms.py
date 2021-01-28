e8486bedce2634024b3b756c372dd15b184e3deb
Sharat Doddaghatta <schandra2@godaddy.com>
2021-01-27 17:33:36 -0800

    27-jan

---
+++ educative.io/merge_intervals/4_minimum_meeting_rooms.py
@@ -0,0 +1,48 @@
+# Given a list of intervals representing the start and end time of ‘N’ meetings, find the minimum number of rooms
+# required to hold all the meetings.
+
+from heapq import *
+
+class Meeting:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def __lt__(self, other):
+        return self.end < other.end
+
+
+def min_meeting_rooms(meetings):
+    meetings.sort(key=lambda x: x.start)
+
+    min_heap = [meetings[0]]
+    min_rooms = 0
+
+    for meeting in meetings:
+        while len(min_heap) > 0 and meeting.start >= min_heap[0].end:
+            heappop(min_heap)
+
+        heappush(min_heap, meeting)
+
+        min_rooms = max(min_rooms, len(min_heap))
+
+    return min_rooms
+
+
+def min_meeting_rooms2(meetings):
+    start_times = [meeting.start for meeting in meetings]
+    end_times = [meeting.end for meeting in meetings]
+    i = 0
+    start_times.sort()
+    end_times.sort()
+    concurrent_events = 0
+    min_rooms = 0
+    while i < len(start_times):
+        if start_times[i] < end_times[j]:
+            concurrent_events += 1
+            i += 1
+            min_rooms = max(concurrent_events, min_rooms)
+        else:
+            concurrent_events -= 1
+            j += 1
+    return min_rooms
---
+++ educative.io/merge_intervals/6_employee_free_time.py
@@ -0,0 +1,77 @@
+from heapq import *
+
+
+class Interval:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def print_interval(self):
+        print("[" + str(self.start) + ", " + str(self.end) + "]", end='')
+
+
+class EmployeeInterval:
+    def __init__(self, interval, employeeIndex, intervalIndex):
+        self.interval = interval
+
+        self.employeeIndex = employeeIndex
+        self.intervalIndex = intervalIndex  # index of the interval in the employee list
+
+    def __lt__(self, other):
+        # min heap based on meeting.end
+        return self.interval.start < other.interval.start
+
+
+def find_employee_free_time(schedule):
+    min_heap = []
+    result = []
+    for i in range(len(schedule)):
+        heappush(min_heap, EmployeeInterval(schedule[i][0], i, 0))
+
+    previous_interval = min_heap[0].interval
+
+    while min_heap:
+        current_interval = heappop(min_heap)
+
+        if previous_interval.end < current_interval.interval.start:
+            result.append(Interval(previous_interval.end, current_interval.interval.start))
+            previous_interval = current_interval.interval
+        else:
+            if previous_interval.end < current_interval.end:
+                previous_interval = current_interval.interval
+
+        employee_schedule = schedule[current_interval.employeeIndex]
+        if len(employee_schedule) > current_interval.intervalIndex + 1:
+            heappush(min_heap, EmployeeInterval(employee_schedule[current_interval.intervalIndex + 1],
+                                                current_interval.employeeIndex,
+                                                current_interval.intervalIndex + 1))
+
+    return result
+
+
+def main():
+
+    input = [[Interval(1, 3), Interval(5, 6)], [
+        Interval(2, 3), Interval(6, 8)]]
+    print("Free intervals: ", end='')
+    for interval in find_employee_free_time(input):
+        interval.print_interval()
+    print()
+
+    input = [[Interval(1, 3), Interval(9, 12)], [
+        Interval(2, 4)], [Interval(6, 8)]]
+    print("Free intervals: ", end='')
+    for interval in find_employee_free_time(input):
+        interval.print_interval()
+    print()
+
+    input = [[Interval(1, 3)], [
+        Interval(2, 4)], [Interval(3, 5), Interval(7, 9)]]
+    print("Free intervals: ", end='')
+    for interval in find_employee_free_time(input):
+        interval.print_interval()
+    print()
+
+
+if __name__ == "__main__":
+    main()
