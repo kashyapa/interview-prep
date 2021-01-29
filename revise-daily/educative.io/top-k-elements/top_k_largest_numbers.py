02a449d3fa08aab9b69fe6aeb00603b1e2a8d2aa
Sharat Doddaghatta <schandra2@godaddy.com>
2021-01-29 15:32:36 -0800

    29-jan

--- revise-daily/educative.io/top-k-elements/top_k_largest_numbers.py
+++ revise-daily/educative.io/top-k-elements/top_k_largest_numbers.py
@@ -3,4 +3,16 @@
 
 from heapq import *
 
+
 def find_k_largest_numbers(nums, k):
+    min_heap = []
+
+    for i in range(k):
+        heappush(min_heap, nums[i])
+
+    for i in range(k, len(nums)):
+        if min_heap[0] < nums[i]:
+            heappush(min_heap, nums[i])
+            heappop(min_heap)
+
+    return min_heap
