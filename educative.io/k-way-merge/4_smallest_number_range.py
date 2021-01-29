801323d4165f39222239564567f3830a621004ec
Sharat Doddaghatta <schandra2@godaddy.com>
2021-01-28 22:09:06 -0800

    28-jan

--- educative.io/k-way-merge/4_smallest_number_range.py
+++ educative.io/k-way-merge/4_smallest_number_range.py
@@ -5,22 +5,23 @@ from heapq import *
 
 
 def find_smallest_range(lists):
-    min_heap = []
-    current_max = -inf
     range_start, range_end = 0, inf
+    current_max = -inf
+    min_heap = []
 
-    for i in range(len(lists)):
-        current_max = max(current_max, lists[i][0])
-        heappush(min_heap, (lists[i][0], i, lists[i]))
+    for i, list in enumerate(lists):
+        heappush(min_heap, (list[i], i, list))
+        current_max = max(current_max, list[i])
 
     while min_heap:
-        number, i, list = heappop(min_heap)
-        if range_end - range_start > current_max - number:
-            range_start = number
+        num, i, lst = heappop(min_heap)
+
+        if range_end - range_start > current_max - num:
+            range_start = num
             range_end = current_max
 
-        if i+1 < len(list):
-            heappush(min_heap, list[i+1], i+1, list)
-            current_max = max(list[i+1], current_max)
+        if i+1 < len(lst):
+            heappush(min_heap, (lst[i+1], i+1, lst))
+            current_max = max(current_max, lst[i+1])
 
     return [range_start, range_end]
