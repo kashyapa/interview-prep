e19391bbb8ee0fd977966260776c2cb9fdee1fd2
Sharat Doddaghatta <schandra2@godaddy.com>
2021-01-28 20:10:16 -0800

    28-jan

--- educative.io/k-way-merge/5_k_pairs_with_largest_sum.py
+++ educative.io/k-way-merge/5_k_pairs_with_largest_sum.py
@@ -6,19 +6,16 @@ from heapq import *
 
 def k_pairs_with_largest_sum(nums1, nums2, k):
     min_heap = []
-
-    for i in range(0, min(k, len(nums1))):
-        for j in range(0, min(k, len(nums2))):
+    for i in range(len(nums1)):
+        for j in range(len(nums2)):
             if len(min_heap) < k:
-                heappush(nums1[i] + nums2[j], i, j)
+                heappush(min_heap, (nums1[i] + nums2[j], i, j))
             else:
                 if nums1[i] + nums2[j] < min_heap[0][0]:
                     break
                 else:
                     heappop(min_heap)
-                    heappush(nums1[i] + nums2[j], i, j)
+                    heappush(min_heap, (nums1[i] + nums2[j], i, j))
     result = []
-    for (num, i, j) in min_heap:
-        result.append([nums1[i], nums2[j]])
-
-    return result
+    for num, i, j in heappop(min_heap):
+        result.append([i, j])
