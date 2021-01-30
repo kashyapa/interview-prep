02c8b9295a707192d2cd13a0c7972f876182dea7
Sharat Doddaghatta <schandra2@godaddy.com>
2021-01-29 23:40:40 -0800

    29-jan

---
+++ revise-daily/educative.io/top-k-elements/5_top_k_frequent_numbers.py
@@ -0,0 +1,30 @@
+# Given an unsorted array of numbers, find the top ‘K’ frequently occurring numbers in it.
+
+from heapq import *
+from collections import Counter
+
+
+def find_k_frequent_numbers(nums, k):
+    topNumbers = []
+    min_heap = []
+    count = Counter(nums)
+
+    for k, v in count:
+        heappush(min_heap, v)
+        if len(min_heap) > k:
+            heappop(min_heap)
+
+    while min_heap:
+        topNumbers.append(heappop(min_heap[1]))
+
+    return topNumbers
+
+
+def main():
+
+    print("Here are the K frequent numbers: " + str(find_k_frequent_numbers([1, 3, 5, 12, 11, 12, 11], 2)))
+    print("Here are the K frequent numbers: " + str(find_k_frequent_numbers([5, 12, 11, 3, 11], 2)))
+
+
+if __name__ == '__main__':
+    main()
