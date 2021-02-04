51e7dd207dc10ab44ea44314579ee7e0835bc3b6
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-04 10:58:00 -0800

    2-2-2021

---
+++ revise-daily/educative.io/subsets/7_find_unique_trees.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+def find_unique_trees(n):
+    if n <= 0:
+        return []
+    return findUnique_trees_recursive(1, n)
+
+
+def findUnique_trees_recursive(start, end):
+    result = []
+
+    if start > end:
+        result.append(None)
+        return result
+
+    for i in range(start, end + 1):
+        left_trees = findUnique_trees_recursive(start, i-1)
+        right_trees = findUnique_trees_recursive(i+1, end)
+
+        for left in left_trees:
+            for right in right_trees:
+                root = TreeNode(i)
+                root.left = left
+                root.right = right
+                result.append(root)
+
+    return result
+def main():
+    print("Total trees: " + str(len(find_unique_trees(2))))
+    print("Total trees: " + str(len(find_unique_trees(3))))
+
+
+main()
