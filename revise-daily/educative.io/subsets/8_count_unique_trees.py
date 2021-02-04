5c5272f825685f7ce77be2dd2f2638e8acc8ea69
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-04 11:23:38 -0800

    2-2-2021

--- revise-daily/educative.io/subsets/8_count_unique_trees.py
+++ revise-daily/educative.io/subsets/8_count_unique_trees.py
@@ -26,6 +26,23 @@ def count_trees(n):
     return count
 
 
+def count_trees_rec(map, n):
+    if n in map:
+        return map[n]
+
+    if n <= 1:
+        return 1
+    count = 0
+    for i in range(1, n + 1):
+        # making 'i' the root of the tree
+        countOfLeftSubtrees = count_trees_rec(map, i - 1)
+        countOfRightSubtrees = count_trees_rec(map, n - i)
+        count += (countOfLeftSubtrees * countOfRightSubtrees)
+
+    map[n] = count
+    return count
+
+
 def main():
     print("Total trees: " + str(count_trees(2)))
     print("Total trees: " + str(count_trees(3)))
