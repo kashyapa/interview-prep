ee1c925a3d0f9d3deb61e3f2e11c0d55f60091fc
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-03 18:15:03 -0800

    2-2-2021

---
+++ revise-daily/educative.io/subsets/3_permutations.py
@@ -0,0 +1,44 @@
+# Given a set of distinct numbers, find all of its permutations.
+#
+# Permutation is defined as the re-arranging of the elements of the set. For example, {1, 2, 3} has the following six permutations:
+#
+# {1, 2, 3}
+# {1, 3, 2}
+# {2, 1, 3}
+# {2, 3, 1}
+# {3, 1, 2}
+# {3, 2, 1}
+# If a set has ‘n’ distinct elements it will have n!n! permutations.
+
+# Input: [1,3,5]
+# Output: [1,3,5], [1,5,3], [3,1,5], [3,5,1], [5,1,3], [5,3,1]
+
+
+from collections import deque
+
+
+def find_permutations(nums):
+    result = []
+    permutations = deque()
+    permutations.append([])
+
+    for i in range(len(nums)):
+
+        old_perm = permutations.popleft()
+
+        for j in range(len(old_perm)):
+            new_perm = list(old_perm)
+            new_perm.insert(j, nums[i])
+            if len(new_perm) == len(nums):
+                result.append(new_perm)
+            else:
+                permutations.append(new_perm)
+
+    return result
+
+
+def main():
+    print("Here are all the permutations: " + str(find_permutations([1, 3, 5])))
+
+
+main()
