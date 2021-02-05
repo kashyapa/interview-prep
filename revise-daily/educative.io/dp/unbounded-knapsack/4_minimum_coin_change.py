66e88b2f68da4133f486b4e93c8a062d25075c77
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-05 12:07:41 -0800

    2-5-2021

--- revise-daily/educative.io/dp/unbounded-knapsack/4_minimum_coin_change.py
+++ revise-daily/educative.io/dp/unbounded-knapsack/4_minimum_coin_change.py
@@ -34,6 +34,26 @@ def count_change_recursive(dp, denominations, total, currentIndex):
     return dp[currentIndex][total]
 
 
+def count_ribbon_pieces_dp(ribbonLengths, total):
+    n = len(ribbonLengths)
+    dp = [[-math.inf for _ in range(total + 1)] for _ in range(n)]
+
+    # populate the total=0 columns, as we don't need any ribbon to make zero total
+    for i in range(n):
+        dp[i][0] = 0
+
+    for i in range(n):
+        for t in range(1, total + 1):
+            if i > 0:  # exclude the ribbon
+                dp[i][t] = dp[i - 1][t]
+            # include the ribbon and check if the remaining length can be cut into available lengths
+            if t >= ribbonLengths[i] and dp[i][t - ribbonLengths[i]] != -math.inf:
+                dp[i][t] = max(dp[i][t], dp[i][t - ribbonLengths[i]] + 1)
+
+    # total combinations will be at the bottom-right corner, return '-1' if cutting is not possible
+    return -1 if dp[n - 1][total] == -math.inf else dp[n - 1][total]
+
+
 def main():
     print(count_change([1, 2, 3], 5))
     print(count_change([1, 2, 3], 11))
