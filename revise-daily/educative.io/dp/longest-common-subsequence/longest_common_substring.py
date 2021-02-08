99bfae825d2bc5ba3c1916b7d12a5ac186b91061
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-08 01:00:35 -0800

    2-5-2021

---
+++ revise-daily/educative.io/dp/longest-common-subsequence/longest_common_substring.py
@@ -0,0 +1,26 @@
+def find_LCS_length(s1, s2):
+    return find_lcs_rec(s1, s2, 0, 0, 0)
+
+
+def find_lcs_rec(s1, s2, i1, i2, count):
+
+    if i1 == len(s1) or i2 == len(s2):
+        return count
+    c1 = 0
+    if s1[i1] == s2[i2]:
+        c1 = find_lcs_rec(s1,s2, i1+1, i2+1, count+1)
+
+    c2 = find_lcs_rec(s1, s2, i1, i2+1, 0)
+    c3 = find_lcs_rec(s1, s2, i1+1, i2, 0)
+
+    return max(c1, max(c2,c3))
+
+
+
+def main():
+    print(find_LCS_length("abdca", "cbda"))
+    print(find_LCS_length("passport", "ppsspt"))
+
+
+if __name__ == "__main__":
+    main()
