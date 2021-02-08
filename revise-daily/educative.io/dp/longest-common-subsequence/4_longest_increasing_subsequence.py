278ebceac8be8d6fa64b55727719855448fb938b
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-08 14:44:23 -0800

    2-5-2021

--- revise-daily/educative.io/dp/longest-common-subsequence/4_longest_increasing_subsequence.py
+++ revise-daily/educative.io/dp/longest-common-subsequence/4_longest_increasing_subsequence.py
@@ -6,6 +6,20 @@
 # Output: 5
 # Explanation: The LIS is {2,3,6,10,12}.
 
+def find_LIS(arr):
+    return find_LIS_rec(arr, 0, -1)
+
+def find_LIS_rec(arr, current_index, prev_index):
+    if current_index == len(arr):
+        return 0
+    c1 = 0
+    if prev_index == -1 or arr[current_index] > arr[prev_index]:
+        c1 = 1 + find_LIS_rec(arr, current_index+1, current_index)
+
+    c2 = find_LIS_rec(arr, current_index+1, prev_index)
+
+    return max(c1, c2)
+
 def find_LIS_length(arr):
     n1 = len(arr)
     dp = [1 for _ in range(n1)]
@@ -23,6 +37,8 @@ def find_LIS_length(arr):
 def main():
     print(find_LIS_length([4, 2, 3, 6, 10, 1, 12]))
     print(find_LIS_length([-4, 10, 3, 7, 15]))
+    print(find_LIS([4, 2, 3, 6, 10, 1, 12]))
+    print(find_LIS([-4, 10, 3, 7, 15]))
 
 
 if __name__ == "__main__":
--- revise-daily/educative.io/dp/longest-common-subsequence/5_maximum_sum_increasing_subsequence.py
+++ revise-daily/educative.io/dp/longest-common-subsequence/5_maximum_sum_increasing_subsequence.py
@@ -6,10 +6,22 @@
 # Explanation: The increaseing sequence is {4,6,10,12}.
 # Please note the difference, as the LIS is {1,2,6,10,12} which has a sum of '31'.
 
-def find_MSIS(arr):
-    return find_MSIS_rec(arr, 0, 0)
 
-def find_MSIS_rec(arr, i, sum):
+def find_MSIS(arr):
+    return find_MSIS_rec(arr, 0, -1, 0)
+
+
+def find_MSIS_rec(arr, current_index, prev_index, sum):
+    if current_index >= len(arr):
+        return sum
+
+    s1 = sum
+    if prev_index == -1 or arr[current_index] > arr[prev_index]:
+        s1 = find_MSIS_rec(arr, current_index+1, current_index, sum+arr[current_index])
+
+    s2 = find_MSIS_rec(arr, current_index+1, prev_index, sum)
+
+    return max(s1, s2)
 
 
 def main():
@@ -17,4 +29,5 @@ def main():
     print(find_MSIS([-4, 10, 3, 7, 15]))
 
 
-main()
+if __name__ == "__main__":
+    main()
