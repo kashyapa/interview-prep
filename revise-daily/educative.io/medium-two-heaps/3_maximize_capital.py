524aaf19a6ca1a8a2438d919081e7792feccb448
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-09 19:48:38 -0800

    2-5-2021

--- revise-daily/educative.io/medium-two-heaps/3_maximize_capital.py
+++ revise-daily/educative.io/medium-two-heaps/3_maximize_capital.py
@@ -1,8 +1,16 @@
 from heapq import *
 
 
-def find_maximum_capital(capital, profits, numberOfProjects, initialCapital):
+# Given a set of investment projects with their respective profits, we need to find the most profitable projects.
+# We are given an initial capital and are allowed to invest only in a fixed number of projects. Our goal is to choose
+# projects that give us the maximum profit. Write a function that returns the maximum total capital after selecting the
+# most profitable projects.
+#
+# We can start an investment project only when we have the required capital. Once a project is selected, we can assume
+# that its profit has become our capital.
 
+
+def find_maximum_capital(capital, profits, numberOfProjects, initialCapital):
     max_heap = []
     min_heap = []
 
