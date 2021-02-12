e973e8aef6dd9dfc95b99b1ae47c4b32b7ae662d
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-11 21:54:49 -0800

    2-13-2021

---
+++ revise-daily/educative.io/easy-cyclic-sort/4_find_duplicate_number.py
@@ -0,0 +1,43 @@
+# We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’.
+# The array has only one duplicate but it can be repeated multiple times. Find that duplicate number without using any
+# extra space. You are, however, allowed to modify the input array.
+
+# Example 1:
+#
+# Input: [1, 4, 4, 3, 2]
+# Output: 4
+# Example 2:
+#
+# Input: [2, 1, 3, 3, 5, 4]
+# Output: 3
+# Example 3:
+#
+# Input: [2, 4, 1, 4, 4]
+# Output: 4
+
+
+def find_duplicate(nums):
+
+    i = 0
+    while i < len(nums):
+        adjusted_index = nums[i] - 1
+        if nums[i] != nums[adjusted_index]:
+            nums[i], nums[adjusted_index] = nums[adjusted_index], nums[i]
+        else:
+            i += 1
+
+    for i in range(len(nums)):
+        if nums[i] != i+1:
+            return nums[i]
+
+    return -1
+
+
+def main():
+    print(find_duplicate([1, 4, 4, 3, 2]))
+    print(find_duplicate([2, 1, 3, 3, 5, 4]))
+    print(find_duplicate([2, 4, 1, 4, 4]))
+
+
+if __name__ == "__main__":
+    main()
