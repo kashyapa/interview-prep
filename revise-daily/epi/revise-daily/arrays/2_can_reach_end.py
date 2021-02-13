bc43f5d07fb0a8ab8baf66df3c801713abb2b62b
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-13 08:13:56 -0800

    2-13-2021

--- revise-daily/epi/revise-daily/arrays/2_can_reach_end.py
+++ revise-daily/epi/revise-daily/arrays/2_can_reach_end.py
@@ -9,6 +9,17 @@ def can_reach_end(arr):
     return True
 
 
+def can_reach_end2(arr):
+    i = 0
+    max_reachable = 0
+    while i < len(arr):
+        if i > max_reachable:
+            return False
+        max_reachable = max(max_reachable, i + arr[i])
+        i += 1
+    return max_reachable >= len(arr)
+
+
 if __name__ == "__main__":
     a = [2, 4, 1, 1, 0, 2, 3]
     res = can_reach_end(a)
