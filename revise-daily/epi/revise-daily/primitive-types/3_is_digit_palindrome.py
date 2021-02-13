7ca43d25f01c70440f1d90f25b319aabc8d7220d
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-13 07:28:45 -0800

    2-13-2021

--- revise-daily/epi/revise-daily/primitive-types/3_is_digit_palindrome.py
+++ revise-daily/epi/revise-daily/primitive-types/3_is_digit_palindrome.py
@@ -1,6 +1,5 @@
 def is_palindrome(x):
 
-    lsb, msb = 0, 0
     res = 1
 
     while res < x:
@@ -12,3 +11,14 @@ def is_palindrome(x):
         msb = x // div
         if lsb != msb:
             return False
+        x = (x % div)//10
+        div //= 100
+    return True
+
+
+if __name__ == "__main__":
+    print(is_palindrome(323))
+    print(is_palindrome(3323))
+    print(is_palindrome(1323))
+    print(is_palindrome(23323))
+    print(is_palindrome(3))
