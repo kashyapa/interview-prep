3dc9a90d91eab11f68453871f00281ea7b0f64be
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-14 08:39:16 -0800

    2-13-2021

---
+++ revise-daily/epi/revise-daily/arrays/10_pascal_triangle.py
@@ -0,0 +1,11 @@
+def generate_pascal_triangle(n):
+    rows = [[1] * (i+1) for i in range(n)]
+    for i in range(1, n):
+        for j in range(1, i):
+            rows[i][j] = rows[i-1][j-1] + rows[i-1][j]
+
+    return rows
+
+
+if __name__ == '__main__':
+    print(generate_pascal_triangle(6))
