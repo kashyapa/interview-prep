8c72a0a466dbe4b449bf0762f392cd7dd32adc99
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-13 18:10:27 -0800

    2-13-2021

--- revise-daily/epi/revise-daily/arrays/6_delete_duplicates.py
+++ revise-daily/epi/revise-daily/arrays/6_delete_duplicates.py
@@ -1,7 +1,11 @@
 def delete_duplicates(arr):
-    w = 1
+    w = 0
     for i in range(1, len(arr)):
-        if arr[i] != arr[w-1]:
-            arr[w] = arr[i]
+        if arr[i] != arr[i-1]:
+            arr[w] = arr[i-1]
             w += 1
     return w
+
+
+if __name__ == '__main__':
+    print(delete_duplicates([2, 3, 5, 5 ,5 ,7 , 11, 11, 13]))
---
+++ revise-daily/epi/revise-daily/arrays/7_sudoku_checker_problem.py
@@ -0,0 +1,23 @@
+import math
+
+
+def has_duplicate(block):
+    block = filter(lambda x:x !=0, block)
+    return len(block) != len(set(block))
+
+
+def is_valid_sudoku(partial_assignment):
+    n = len(partial_assignment)
+    if any (has_duplicate([partial_assignment[i][j] for j in range(n)]) or
+            has_duplicate([partial_assignment[j][i] for j in range(n)])
+            for i in range(n)):
+        return False
+
+    regions_size = int(math.sqrt(n))
+
+    return all(not has_duplicate(
+                    [partial_assignment[a][b]
+                     for b in range(regions_size * I, regions_size * (I+1))
+                     for a in range(regions_size * J, regions_size * (J+1))
+                     ])
+               for I in range(regions_size) for J in range(regions_size))
