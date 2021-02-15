8368b5a8f0acc38644a83b239ccd29e98391e2e2
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-15 10:59:52 -0800

    2-13-2021

---
+++ revise-daily/epi/revise-daily/strings/11_sinusoidal_wave.py
@@ -0,0 +1,9 @@
+def snake_string(s):
+    result = []
+    for i in range(1, len(s), 4):
+        result.append(s[i])
+    for i in range(0, len(s), 2):
+        result.append(s[i])
+    for i in range(3, len(s), 4)
+        result.append(s[i])
+    return ''.join(result)
