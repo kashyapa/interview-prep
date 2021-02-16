2733d07f47f82d6002f1a486f6659ce42f89025a
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-16 13:44:38 -0800

    2-13-2021

---
+++ revise-daily/epi/revise-daily/3_linked_lists/11_list_pivoting.py
@@ -0,0 +1,54 @@
+class ListNode:
+    def __init__(self, data, p):
+        self.data = data
+        self.next = p
+
+
+def list_pivoting(L: ListNode, x: int):
+
+    head, tail = L, L
+    temp = None
+
+    while L and L.next:
+        if L.data < x:
+            temp = L.next
+            L.next = head
+            head = L
+        else:
+            temp = L.next
+            tail.next = L
+            tail = L
+        L = temp
+    return head
+
+
+if __name__ == '__main__':
+    A = ListNode(6, None)
+    B = ListNode(7, A)
+    C = ListNode(2, B)
+    D = ListNode(4, C)
+    E = ListNode(1, D)
+    F = ListNode(6, E)
+    G = ListNode(7, F)
+    l = []
+    head = G
+    while G:
+        if G is None:
+            break
+        l.append(str(G.data) + "-->")
+        G = G.next
+    l.append("none")
+
+    print(''.join(l))
+
+    head = list_pivoting(head, 3)
+
+    l = []
+    while head:
+        if head is None:
+            break
+        l.append(str(head.data) + "-->")
+        head = head.next
+    l.append("none")
+
+    print(''.join(l))
