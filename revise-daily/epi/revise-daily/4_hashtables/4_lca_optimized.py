d21040da12522c75790f18370f0ae8cd021df0af
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-16 22:38:40 -0800

    2-13-2021

---
+++ revise-daily/epi/revise-daily/4_hashtables/4_lca_optimized.py
@@ -0,0 +1,21 @@
+class BinaryTreeNode:
+    def __init__(self, val, n0, n1, p):
+        self.data = val
+        self.right = None
+        self.left = None
+        self.parent = None
+
+
+def optimized_lca(n0: BinaryTreeNode, n1: BinaryTreeNode):
+    nodes_on_path = set()
+    while n0 or n1:
+        if n0 in nodes_on_path:
+            return n0
+        nodes_on_path.add(n0)
+        n0 = n0.parent
+
+        if n1 in nodes_on_path:
+            return n1
+        nodes_on_path.add(n1)
+        n1 = n1.parent
+    raise ValueError("n0 and n1 not in same tree")
