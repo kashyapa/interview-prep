41b451c8624fcc4cf82dcb5e264875c440139746
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-19 09:47:22 -0800

    2-13-2021

--- revise-daily/epi/revise-daily/5_dynamic_programming/2_levenstein_distance.py
+++ revise-daily/epi/revise-daily/5_dynamic_programming/2_levenstein_distance.py
@@ -17,5 +17,21 @@ def edit_distance_rec(s1, s2, i1, i2):
     return add_character_in_s1 + delete_character_in_s1 + replace_character
 
 
+def edit_distance_dp(s1, s2):
+    def compute_distance_between_prefixes(idx1, idx2):
+        if idx1 < 0:
+            return idx2 +  1
+
+        elif idx2 < 0:
+            return idx1 + 1
+        if distance_between_prefixes[idx1][idx2] == - 1:
+            if s1[idx1] == s2[idx2]:
+                distance_between_prefixes[idx1][idx2] = compute_distance_between_prefixes(idx1-1, idx2-1)
+            else:
+
+    distance_between_prefixes = [[-1] * len(s2) for _ in s1]
+    return compute_distance_between_prefixes(len(s1)-1, len(s2)-1)
+
+
 if __name__ == '__main__':
     print(edit_distance("tabactor", "badactor"))
