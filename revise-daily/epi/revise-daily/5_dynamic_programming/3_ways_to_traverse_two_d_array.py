dd0bbe00e8c296f4cabb1cd60eb36c3f95dcef17
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-19 13:12:09 -0800

    2-13-2021

--- revise-daily/epi/revise-daily/5_dynamic_programming/3_ways_to_traverse_two_d_array.py
+++ revise-daily/epi/revise-daily/5_dynamic_programming/3_ways_to_traverse_two_d_array.py
@@ -9,9 +9,22 @@ def traverse_2_d_array(m, n):
             dp[x][y] = ways_right + ways_top
         return dp[x][y]
 
-    dp = [[0] * (n) for _ in range(m)]
+    dp = [[0] * n for _ in range(m)]
     return traverse_2d_dp(m-1, n-1)
+
+
+def unique_paths_rec(m, n):
+    def unique_paths(x, y):
+        if x == m-1 or y == n-1:
+            return 1
+        if dp[x][y] == 0:
+            dp[x][y] = unique_paths(x + 1, y) + unique_paths(x, y + 1)
+        return dp[x][y]
+
+    dp = [[0] * n for _ in range(m)]
+    return unique_paths(0, 0)
 
 
 if __name__ == '__main__':
     print(traverse_2_d_array(5, 4))
+    print(unique_paths_rec(5, 4))
