1e2f1ba831c9bb8ff2a807f27c3293b795271051
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-19 23:10:24 -0800

    2-13-2021

---
+++ revise-daily/epi/revise-daily/5_dynamic_programming/4_search_sequence_2d_array.py
@@ -0,0 +1,35 @@
+def is_pattern_contained_in_grid(grid, pattern):
+
+    def pattern_search(i, j, k):
+        if k == len(pattern)-1:
+            return True
+
+        if 0 > i >= len(grid) or 0 > j >= len(grid):
+            return False
+
+        if grid[i][j] != pattern[k]:
+            return False
+
+        return any(
+                (pattern_search(i+1, j, k + 1), pattern_search(i, j + 1, k + 1), pattern_search(i-1, j, k + 1),
+                 pattern_search(i, j-1, k + 1))
+        )
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == pattern[0]:
+                if pattern_search(i, j, 0) is True:
+                    return True
+    return False
+
+
+if __name__ == '__main__':
+    grid = [
+        [1, 2, 3, 4, 5],
+        [9, 43, 65, 76, 34],
+        [423, 65645, 723, 22, 765],
+        [978, 45, 98, 324, 345],
+        ]
+    print(is_pattern_contained_in_grid(grid, [65645, 423, 9, 43]))
+    print(is_pattern_contained_in_grid(grid, [65645, 433, 9, 43]))
+
