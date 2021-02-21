cec35ef6878d0616fcda637d6ac5e91a88716305
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-21 15:17:36 -0800

    2-13-2021

--- revise-daily/epi/revise-daily/5_dynamic_programming/5_knapsack.py
+++ revise-daily/epi/revise-daily/5_dynamic_programming/5_knapsack.py
@@ -7,14 +7,14 @@ def knapsack(profits, weights, capacity):
 
         with_item = 0
         if weights[index] <= capacity:
-            with_item = profits[index]+ knapsack_rec(capacity-weights[index])
+            with_item = profits[index] + knapsack_rec(capacity-weights[index], index+1)
 
-        skip_item = knapsack_rec(capacity)
+        skip_item = knapsack_rec(capacity, index+1)
 
         return max(with_item, skip_item)
 
     return knapsack_rec(capacity, 0)
 
 
-
 if __name__ == '__main__':
+    print(knapsack([3, 43, 3434], [2, 5, 1], 3))
