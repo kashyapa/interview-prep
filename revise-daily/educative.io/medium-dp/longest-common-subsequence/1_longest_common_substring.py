9f34293830bdef87611f11b4604aad67d04d0e3d
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-21 01:09:09 -0800

    2-13-2021

--- revise-daily/educative.io/medium-dp/longest-common-subsequence/1_longest_common_substring.py
+++ revise-daily/educative.io/medium-dp/longest-common-subsequence/1_longest_common_substring.py
@@ -7,12 +7,12 @@ def find_lcs_rec(s1, s2, i1, i2, count):
     if i1 == len(s1) or i2 == len(s2):
         return count
     if s1[i1] == s2[i2]:
-        count = find_lcs_rec(s1,s2, i1+1, i2+1, count+1)
+        count = find_lcs_rec(s1, s2, i1+1, i2+1, count+1)
 
     c2 = find_lcs_rec(s1, s2, i1, i2+1, 0)
     c3 = find_lcs_rec(s1, s2, i1+1, i2, 0)
 
-    return max(count, max(c2,c3))
+    return max(count, max(c2, c3))
 
 
 def find_lcs_dp(s1, s2):
