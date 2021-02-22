12905d493c121acd1837576a9511065814a4021f
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-22 12:06:29 -0800

    2-13-2021

---
+++ revise-daily/epi/revise-daily/recursion_and_backtracking/1_phone_mnemonics.py
@@ -0,0 +1,24 @@
+def phone_mnemonics(ph_number):
+
+    def phone_mnemonics_rec(partial_res, index):
+
+        if index == len(ph_number):
+            result.append(''.join(partial_res))
+            return
+
+        s = mapping[int(ph_number[index])]
+        for i in range(len(s)):
+            partial_res.append(s[i])
+            phone_mnemonics_rec(partial_res, index+1)
+            partial_res.pop()
+
+        return
+
+    mapping = ("0", "1", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ")
+    result = []
+    phone_mnemonics_rec([], 0)
+    return result
+
+
+if __name__ == '__main__':
+    print(phone_mnemonics("8583567273"))
