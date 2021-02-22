8a75ce7ebe273633fb33b29a83967fb2e288cf62
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-22 15:53:06 -0800

    2-13-2021

---
+++ revise-daily/epi/revise-daily/recursion_and_backtracking/3_generate_permutations.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+def generate_permutations(arr):
+    result = []
+    permutations = deque()
+    permutations.append([])
+
+    for i in range(len(arr)):
+        for _ in range(len(permutations)):
+            old_perm = permutations.popleft()
+            for j in range(len(old_perm)+1):
+                new_perm = list(old_perm)
+                new_perm.insert(j, arr[i])
+                if len(new_perm) == len(arr):
+                    result.append(new_perm)
+                else:
+                    permutations.append(new_perm)
+
+    return result
+
+
+if __name__ == '__main__':
+    arr = [2, 3, 5, 7]
+    print(generate_permutations(arr))
