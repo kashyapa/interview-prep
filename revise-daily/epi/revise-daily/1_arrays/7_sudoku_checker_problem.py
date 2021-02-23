975a5d52146966d46efa3242ac83a8c7f73a7c07
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-23 12:47:32 -0800

    2/22/2021

--- revise-daily/epi/revise-daily/1_arrays/7_sudoku_checker_problem.py
+++ revise-daily/epi/revise-daily/1_arrays/7_sudoku_checker_problem.py
@@ -2,13 +2,13 @@ import math
 
 
 def has_duplicate(block):
-    block = filter(lambda x:x !=0, block)
+    block = filter(lambda x: x !=0, block)
     return len(block) != len(set(block))
 
 
 def is_valid_sudoku(partial_assignment):
     n = len(partial_assignment)
-    if any (has_duplicate([partial_assignment[i][j] for j in range(n)]) or
+    if any(has_duplicate([partial_assignment[i][j] for j in range(n)]) or
             has_duplicate([partial_assignment[j][i] for j in range(n)])
             for i in range(n)):
         return False
@@ -19,5 +19,4 @@ def is_valid_sudoku(partial_assignment):
                     [partial_assignment[a][b]
                      for b in range(regions_size * I, regions_size * (I+1))
                      for a in range(regions_size * J, regions_size * (J+1))
-                     ])
-               for I in range(regions_size) for J in range(regions_size))
+                     ]) for I in range(regions_size) for J in range(regions_size))
