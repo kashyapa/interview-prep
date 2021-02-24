b0b3aa516543c6c697e35e2c585f5486771197da
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-23 18:33:53 -0800

    2/23/2021

---
+++ revise-daily/epi/revise-daily/6_greedy_algorithms/4_three_sum_problem.py
@@ -0,0 +1,16 @@
+def three_sum_problem(arr: list, target: int):
+
+    def two_sum(arr, sum)
+        i, j = 0, len(arr) - 1
+        while i < j:
+            if arr[i] + arr[j] == sum:
+                return True
+            if arr[i] < arr[j]:
+                i += 1
+            else:
+                j -= 1
+        return False
+    arr.sort()
+    return any(
+        two_sum(arr, target-a) for a in arr
+    )
