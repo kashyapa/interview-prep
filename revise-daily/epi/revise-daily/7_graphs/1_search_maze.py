7a49e50d381bf351efc726f29d3d69f3068aa28e
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-25 10:04:48 -0800

    2/25/2021

---
+++ revise-daily/epi/revise-daily/7_graphs/1_search_maze.py
@@ -0,0 +1,38 @@
+from collections import namedtuple
+
+Coordinate = namedtuple('Coordinate', ('x', 'y'))
+
+
+def search_maze(maze):
+    def search_maze_helper(c):
+        if 0 > c.x >= len(maze) or 0 > c.y >= len(maze[0]):
+            return False
+
+        if c.x == len(maze) - 1 and c.y == len(maze[0]) - 1:
+            return True
+
+        if c in failed_points or c in path:
+            return False
+
+        path.append(c)
+        if any(
+            map(search_maze_helper(
+                map(Coordinate, (c.x+1, c.x-1, c.x, c.x),
+                                (c.y, c.y, c.y+1, c.y-1)
+                    )
+            ))
+        ):
+            return True
+        del path[-1]
+        failed_points.add(c)
+        return False
+
+
+    path = []
+    failed_points = set()
+
+    return search_maze_helper(Coordinate(0, 0))
+
+
+if __name__ == '__main__':
+    search_maze([[]])
---
+++ revise-daily/epi/revise-daily/7_graphs/2_paint_boolean_matrix.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+"""
+    Get neighbors of current coordinate from queue
+    if it is white, flip it
+    Add the flipped coordinate to the queue
+"""
+
+def flip_boolean_matrix(x, y, image):
+
+    color = image[x][y]
+    q = deque([(x, y)])
+    image[x][y] = not image[x][y]
+
+    while q:
+        x, y = q.popleft()
+        for x1, y1, in ((x, y+1), (x, y-1), (x+1, y), (x-1, y)):
+            if 0 <= x1 < len(image) and 0 <= y1 < len(image[x1]) and image[x1][y1] == color:
+                image[x1][y1] = not image[x1][y1]
+                q.append((x1, y1))
+
+
+if __name__ == '__main__':
+    flip_boolean_matrix(2, 3, [[]])
+
