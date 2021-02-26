be7f0d9fc63757a6260c20d3530a1472c91181f1
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-25 21:47:25 -0800

    2/25/2021

---
+++ revise-daily/epi/revise-daily/7_graphs/5_clone_graph.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+
+class GraphVertex:
+
+    def __init__(self, label):
+        self.label = label
+        self.edges = []
+
+
+def clone_graph(graph: GraphVertex):
+    if graph is None:
+        return None
+    q = deque([graph])
+    vertex_map = {graph: GraphVertex(graph.label)}
+
+    while q:
+        p = q.popleft()
+
+        for neighbor in p.edges:
+            if neighbor not in vertex_map:
+                vertex_map[neighbor] = GraphVertex(neighbor.label)
+                q.append(neighbor)
+            vertex_map[p].edges.append(vertex_map[neighbor])
+
+    return vertex_map[graph]
