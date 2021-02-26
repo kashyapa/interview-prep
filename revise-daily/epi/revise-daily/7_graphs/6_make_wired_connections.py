1bbcb58dc6de9a091dd462538f9325d2a4917659
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-25 23:21:57 -0800

    2/25/2021

---
+++ revise-daily/epi/revise-daily/7_graphs/6_make_wired_connections.py
@@ -0,0 +1,26 @@
+class GraphVertex:
+
+    def __init__(self):
+        self.d = -1
+        self.edges = []
+
+from collections import deque
+
+
+def is_any_placement_feasible(graph: list[GraphVertex]):
+
+    def bfs(s):
+        s.d = 0
+        q = deque()
+
+        while q:
+
+            for t in q[0].edges:
+                if t.d == -1:
+                    t.d = q[0].d + 1
+                elif t.d == q[0].d:
+                    return False
+            del q[0]
+        return True
+
+    return all(bfs(v) for v in graph if v.d == -1)
