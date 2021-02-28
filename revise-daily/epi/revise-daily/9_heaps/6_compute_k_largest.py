a28b517f720f53a7c7d65167ed8485e77550c23f
Sharat Doddaghatta <schandra2@godaddy.com>
2021-02-27 19:14:13 -0800

    2/25/2021

---
+++ revise-daily/epi/revise-daily/9_heaps/6_compute_k_largest.py
@@ -0,0 +1,25 @@
+import heapq
+
+
+def k_largest_elements_in_a_heap(A, k):
+    if k <= 0:
+        return []
+    candidate_max_heap = []
+    candidate_max_heap.append((-A[0], 0))
+
+    result = []
+
+    for _ in range(k):
+        val, idx = heapq.heappop(candidate_max_heap)
+
+        result.append(val)
+
+        left_child_idx = idx * 2 + 1
+        if len(A) > left_child_idx
+            heapq.heappush(candidate_max_heap, (-A[left_child_idx], left_child_idx))
+
+        right_child_idx = 2*idx + 2
+        if len(A) > right_child_idx:
+            heapq.heappush(candidate_max_heap, (-A[right_child_idx], right_child_idx))
+
+    return result
