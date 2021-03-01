cc12efc6680d0d1780d1cc192a13f8d95f1c4bf0
Sharat Doddaghatta <schandra2@godaddy.com>
2021-03-01 00:02:09 -0800

    2/27/2021

---
+++ revise-daily/educative.io/breadth-first-search/8_connect_all_level_order_siblings.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+from collections import deque
+
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.next = None
+
+    # tree traversal using 'next' pointer
+    def print_tree(self):
+        print("Traversal using 'next' pointer: ", end='')
+        current = self
+        while current:
+            print(str(current.val) + " ", end='')
+            current = current.next
+
+
+def connect_level_order_siblings(root):
+    queue = deque()
+    queue.append(root)
+
+    previous_node = None
+    current_node = None
+
+    while queue:
+        current_node = queue.popleft()
+
+        if previous_node is not None:
+            previous_node.next = current_node
+        previous_node = current_node
+
+        if current_node.left:
+            queue.append(current_node.left)
+        if current_node.right:
+            queue.append(current_node.right)
+    return root
+
+
+def main():
+    root = TreeNode(12)
+    root.left = TreeNode(7)
+    root.right = TreeNode(1)
+    root.left.left = TreeNode(9)
+    root.right.left = TreeNode(10)
+    root.right.right = TreeNode(5)
+    connect_level_order_siblings(root)
+
+    print("Level order traversal using 'next' pointer: ")
+    root.print_tree()
+
+
+if __name__ == '__main__':
+    main()
