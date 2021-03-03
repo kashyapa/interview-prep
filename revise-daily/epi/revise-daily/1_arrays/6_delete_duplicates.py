7be77e3490a822c580d15bd71efa3be60c43932a
Sharat Doddaghatta <schandra2@godaddy.com>
2021-03-03 11:37:59 -0800

    2/25/2021

--- revise-daily/epi/revise-daily/1_arrays/6_delete_duplicates.py
+++ revise-daily/epi/revise-daily/1_arrays/6_delete_duplicates.py
@@ -7,5 +7,18 @@ def delete_duplicates(arr):
     return w
 
 
+
+def delete_dups(arr):
+
+    write_index = 1
+    for i in range(1, len(arr)):
+
+        if arr[i] != arr[write_index-1]:
+            arr[write_index] = arr[i]
+            write_index += 1
+    return arr[:write_index]
+
+
 if __name__ == '__main__':
     print(delete_duplicates([2, 3, 5, 5 ,5 ,7 , 11, 11, 13]))
+    print(delete_dups([2, 3, 5, 5 ,5 ,7 , 11, 11, 13]))
