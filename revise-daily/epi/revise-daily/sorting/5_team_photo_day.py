96b82e98283b259f233d56e799fe864a32904583
Sharat Doddaghatta <schandra2@godaddy.com>
2021-03-04 20:32:26 -0800

    2/25/2021

---
+++ revise-daily/epi/revise-daily/sorting/5_team_photo_day.py
@@ -0,0 +1,14 @@
+from collections import namedtuple
+
+
+class Team:
+    Player = namedtuple('Player', ('height'))
+
+    def __init__(self, height):
+        self._players = [Team.Player(h) for h in height]
+
+    @staticmethod
+    def valid_placement_exists(team0: 'Team', team1: 'Team'):
+        return all(
+            a < b for a, b in zip(sorted(team0._players), sorted(team1._players))
+        )
